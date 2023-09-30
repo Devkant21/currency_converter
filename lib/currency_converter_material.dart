6f57f4d318394f93d351f9222cee5f2170d9ef7f
Devkant <devkant21@gmail.com>
2023-10-01 01:59:34 +0530

    currencyconverter created

---
+++ lib/currency_converter_material.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterial extends StatelessWidget {
+  const CurrencyConverterMaterial({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Currency Converter"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
+import 'package:flp1/currency_converter_material.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(app)
+  runApp(const MyApp());
+}
+
+// Types of widgets
+
+// 1. StatelessWidget - immutable widget
+// 2. StatefulWidget - mutable widget
+// 3. InheritedWidget
+
+// Material Design
+// Cupertino Design
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CurrencyConverterMaterial(),
+    );
+  }
 }
