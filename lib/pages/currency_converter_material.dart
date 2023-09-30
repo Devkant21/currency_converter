80d8f506427201031dfb35a0ccf6f15b337972cd
Devkant <devkant21@gmail.com>
2023-10-01 03:01:19 +0530

    scaffolding fixed

--- lib/pages/currency_converter_material.dart
+++ lib/pages/currency_converter_material.dart
@@ -4,19 +4,42 @@ class CurrencyConverterMaterial extends StatelessWidget {
   const CurrencyConverterMaterial({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(102, 122, 125, 1),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(102, 122, 125, 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "hi there",
+            const Text(
+              "0.00",
               style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.w500,
-                  color: Color.fromRGBO(104, 238, 162, 1)),
-            )
+                fontSize: 35,
+                fontWeight: FontWeight.w500,
+                color: Color.fromRGBO(104, 238, 162, 1),
+              ),
+            ),
+            TextField(
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: "Please enter an amount in USD",
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: const Icon(Icons.monetization_on),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    width: 2.0,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
           ],
         ),
       ),
